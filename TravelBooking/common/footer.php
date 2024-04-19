f04c46d94c171633b240bbb5d97e741e5742c1b2
Siddartha-pandi <153148772+Siddartha-pandi@users.noreply.github.com>
2024-04-19 16:03:06 +0530

    Add files via upload

---
+++ TravelBooking/common/footer.php
@@ -0,0 +1,76 @@
+
+					
+				<div class="footer col-sm-12">
+					
+					<div class="col-sm-4">
+						
+						<div class="footerHeading">
+							Contact Us
+						</div>
+							
+						<div class="footerText">
+							3rd floor, gayatri boys hostel <br> peddarushikonda, vishakaptnam, India
+						</div>
+				
+						<div class="footerText">
+							E-mail: querie@travelwale.com
+						</div>
+						
+					</div>
+					
+					<div class="col-sm-4">
+					
+						<div class="footerHeading">
+							Made with
+						</div>
+						
+						<div class="fa fa-heart"></div>
+						
+						<div class="footerHeading">
+							in India
+						</div>
+						
+						<div class="flagContainer text-center">
+							<img src="images/flag.png">
+						</div>
+						
+					</div>
+					
+					<div class="col-sm-4">
+					
+						<div class="footerHeading">
+							Social Links
+						</div>
+						
+						<div class="socialLinks">
+						
+							<div class="fb">
+								facebook.com/travelwale
+							</div>
+						
+							<div class="gp">
+								plus.google.com/travelwale
+							</div>
+						
+							<div class="tw">
+								twitter.com/travelwale
+							</div>
+						
+							<div class="in">
+								linkedin.com/travelwale
+							</div>
+						
+						</div> 
+						
+					</div>
+						
+					<div class="col-sm-12">
+					<div class="copyrightContainer">
+						<div class="copyright">
+						Copyright &copy; 2024 ,siddartha.
+						</div>
+					</div>
+					</div>
+							
+				</div> 
+		
